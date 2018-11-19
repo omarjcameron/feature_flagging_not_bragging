347f545c8f2df7d6479a6a534666ddec814d8ba5
Omar Cameron <omarjcameron@gmail.com>
2018-11-19 15:52:41 -0800

    Add app files for favorite foods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LDClient from 'ldclient-js';
+
+const isNewer = (a, b) => Date.parse(a.added) < Date.parse(b.added)
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      sortOrder: null,
+      foods: [
+        { name: 'Pepper Pot', added: '2018-10-13'},
+        { name: 'Cacio e Pepe', added: '2018-10-23'},
+        { name: 'Pizza', added: '2018-11-07'},
+        { name: 'Shakshuka', added: '2018-12-31'},
+        { name: 'Tacos al Pastor', added: '2018-12-14'},
+      ]
+    }
+  }
+  componentDidMount() {
+    const user = {
+      key: 'ojc'
+    }
+    this.ldclient = LDClient.initialize('5bee718af7527e4b8311e181', user)
+    this.ldclient.on('ready', this.onLaunchDarklyReady.bind(this))
+    this.ldclient.on('change', this.onLaunchDarklyReady.bind(this))
+  }
+  onLaunchDarklyReady() {
+    this.setState({
+      featureFlags: {
+        defaultSortingIsAdded: this.ldclient.variation('food-favorites-primary-sorting-method')
+      }
+    })
+  }
+
   render() {
+    if (!this.state.featureFlags) {
+        return <div className="App">Loading...</div>
+    }
+
+    let sorter
+    if (this.state.selectedSortOrder) {
+      if (this.state.selectedSortOrder === 'added') {
+        sorter = isNewer;
+      } else if (this.state.selectedSortOrder === 'basic') {
+        sorter = undefined;
+      }
+    } else {
+      if (this.state.featureFlags.defaultSortingIsAdded) {
+        sorter = isNewer;
+      } else {
+        sorter = undefined;
+      }
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div
+          style={{ fontWeight: sorter === undefined ? 'bold' : 'normal'}}
+          onClick={() => this.setState({ selectedSortOrder: 'basic' })}>
+          Basic Sort
+        </div>
+        <div
+          style={{ fontWeight: sorter === isNewer ? 'bold' : 'normal'}}
+          onClick={() => this.setState({ selectedSortOrder: 'added'})}>
+          Sort by date
+        </div>
+        <ul>
+          {this.state.foods.slice().sort(sorter).map(food =>
+              <li>{food.name}</li>
+          )}
+        </ul>
       </div>
     );
   }
